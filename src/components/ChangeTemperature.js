faf6346ab6c217353e9745558ea7b25100bccc99
nnijat <nn.aca2019@gmail.com>
2019-09-06 15:19:27 -0500

    Updated a termostat min value

--- src/components/ChangeTemperature.js
+++ src/components/ChangeTemperature.js
@@ -8,7 +8,7 @@ function ChangeTemperature(props) {
       <label>Change Temp -  Enter a value from 1-100<br />
         <input onChange={(e) => {
           store.dispatch({ type: "SET_TEMP", value: e.target.value })
-        }} type="number" min="0" max="100" />
+        }} type="number" min="1" max="100" />
       </label>
     </div>
   )
